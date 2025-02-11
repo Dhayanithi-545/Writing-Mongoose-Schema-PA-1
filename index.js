aa7932de52f7ec9b73ebe250276acbb59ff95eab
Dhayanithi <545nithi@gmail.com>
2025-02-11 12:02:19 +0530

    Mongoose schemea PA-1 Completed

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
-const express = require('express');
-const { resolve } = require('path');
+const express = require("express");
+const { resolve } = require("path");
 
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(resolve(__dirname, "pages/index.html"));
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+const mongoose = require("mongoose");
+const User = require("./schema.js");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mydatabase", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("Connection error", err));
+
+const testUserSchema = async () => {
+  try {
+    const newUser = new User({
+      username: "dhaya",
+      email: "dhayanithi@gmail.com",
+      password: "dhaya@123456",
+      profile: {
+        firstName: "Dhayanithi",
+        lastName: "A",
+        age: 18,
+      },
+    });
+
+    await newUser.save();
+    console.log("User saved successfully:", newUser);
+  } catch (error) {
+    console.error("Error saving user:", error);
+  }
+};
+
+testUserSchema();
